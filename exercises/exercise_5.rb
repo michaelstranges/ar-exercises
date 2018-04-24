fde9ad464242f0894fe213d5ab9d96d02eeebc47
Michael Stranges <strangesm@gmail.com>
2018-04-24 22:51:27 +0000

    exercise 5 done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+ puts "Total Revenue #{total_revenue}"
+
+@allStores = Store.all
+
+@allStores.each do |store|
+  puts "#{store.name} made #{store.annual_revenue}"
+end
+
+@list = Store.where("annual_revenue > 1000000")
+  @list.each do |store|
+    puts "#{store.name}"
+  end
